7e6e59d0a606d073aa27b6db51bca7c45557a00b
aramzham <aramzham@hotmail.com>
2017-04-13 22:12:35 +0400

    element to element binding

---
+++ DataBinding/ElementToElementBinding/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace ElementToElementBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SampleTextBlock.FontSize = 2;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SampleTextBlock.FontSize = this.FontSize;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SampleTextBlock.FontSize = 40;
+        }
+    }
+}
