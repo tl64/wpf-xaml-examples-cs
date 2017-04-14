7349316b7b1683e443fe3234122ebdeecbb1fbbc
aramzham <aramzham@hotmail.com>
2017-04-14 18:02:23 +0400

    template

---
+++ DataBinding/ConvertValues/Converter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ConvertValues
+{
+    public class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var number = System.Convert.ToInt32(value);
+            switch (number)
+            {
+                case 0:
+                    return "Zero";
+                case 1:
+                    return "One";
+                case 2:
+                    return "Two";
+                case 3:
+                    return "Three";
+                case 4:
+                    return "Four";
+                case 5:
+                    return "Five";
+                default:
+                    return "Error";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var name = value?.ToString();
+            switch (name)
+            {
+                case "Zero":
+                    return 0;
+                case "One":
+                    return 1;
+                case "Two":
+                    return 2;
+                case "Three":
+                    return 3;
+                case "Four":
+                    return 4;
+                case "Five":
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ DataBinding/DataTemplate/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace DataTemplate
+{
+    public class Person
+    {
+        public int Age { get; set; } 
+        public string FirstName { get; set; } 
+        public string LastName { get; set; } 
+        public string Position { get; set; }
+
+        public Person(int age, string firstName, string lastName, string position)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+        }
+    }
+
+    public class PersonList : List<Person>
+    {
+        public PersonList()
+        {
+            this.Add(new Person(20,"Adam", "Evans", "blogger"));
+            this.Add(new Person(34,"Derek", "Alvarado", "trainer"));
+            this.Add(new Person(24,"John", "Stockton", "fighter"));
+            this.Add(new Person(27,"Ebecilio", "Martinez", "dumper"));
+            this.Add(new Person(55,"Kerry", "Jones", "starter"));
+        }
+    }
+}
