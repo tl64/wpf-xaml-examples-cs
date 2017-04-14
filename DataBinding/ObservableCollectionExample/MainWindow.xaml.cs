2d2ff07ee92b57675cb4b5faac86701d8ddd6a28
aramzham <aramzham@hotmail.com>
2017-04-14 11:00:18 +0400

    observable collection

---
+++ DataBinding/ObservableCollectionExample/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObservableCollectionExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> observable = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var debut = new[] { "One", "Two", "Three" };
+            foreach (var x in debut)
+            {
+                observable.Add(x);
+            }
+
+            MyListBox.ItemsSource = observable;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //with List you won't see added items on screen as List doesn't have an event on added item
+            observable.Add(DateTime.Now.ToLongTimeString());
+        }
+    }
+}
