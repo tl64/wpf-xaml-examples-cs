6ca945310d0226765bce63d73c626189b779fc03
aramzham <aramzham@hotmail.com>
2017-04-14 09:38:18 +0400

    binding from code

---
+++ DataBinding/BindingFromCode/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BindingFromCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var binding = new Binding
+            {
+                ElementName = "Slider",
+                Path = new PropertyPath("Value"),
+                Mode = BindingMode.OneWay
+            };
+            SampleTextBlock.SetBinding(TextBlock.FontSizeProperty, binding);
+        }
+    }
+}
